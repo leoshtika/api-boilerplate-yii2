0c305ba172d3d51f65c12a7181ba24d80e9ad816
Leonard Shtika <leoshtika@gmail.com>
2015-09-28 23:43:48 +0300

    Add Note CRUD

---
+++ backend/views/note/index.php
@@ -0,0 +1,40 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel backend\models\NoteSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Notes';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="note-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a('Create Note', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'title',
+            'user_id',
+            'color',
+            'status',
+            // 'created_at',
+            // 'updated_at',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+
+</div>
