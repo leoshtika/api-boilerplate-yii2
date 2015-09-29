e800f4fb33fce6d19e003e0abdabdbdce2ca3b5d
Leonard Shtika <leoshtika@gmail.com>
2015-09-29 22:17:24 +0300

    Add Note's menu

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -40,6 +40,7 @@ AppAsset::register($this);
     if (Yii::$app->user->isGuest) {
         $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
     } else {
+        $menuItems[] = ['label' => 'Notes', 'url' => ['/note/index']];
         $menuItems[] = [
             'label' => 'Logout (' . Yii::$app->user->identity->username . ')',
             'url' => ['/site/logout'],
